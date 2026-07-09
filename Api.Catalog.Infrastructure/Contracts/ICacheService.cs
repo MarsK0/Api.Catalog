0e4fed1df852e3bfc2c27e0f9de05e81f8928373
Marco S Haag <marcosilhaag31@gmail.com>
2026-07-09 17:37:43 -0300

    :building_construction: Abstrai serviço de cache. Criação de interface ICacheService

---
+++ Api.Catalog.Infrastructure/Contracts/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Catalog.Infrastructure.Contracts;
+
+public record CacheOptions(
+    DateTimeOffset? AbsoluteExpiration = null,
+    TimeSpan? RelativeExpiration = null
+);
+public interface ICacheService
+{
+    Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, CacheOptions? options = null);
+    Task<T?> GetAsync<T>(string key);
+    Task SetAsync<T>(string key, T value, CacheOptions? options = null);
+    Task RemoveAsync(string key);
+}
