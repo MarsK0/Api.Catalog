4c6144d93a76e259aace1205605f3e29bd8f55cc
Marco <marcosilhaag31@gmail.com>
2026-07-10 16:18:11 -0300

    :recycle: refatoraçao contexto de tenant #2

--- Api.Catalog.Api/Contexts/TenantContext.cs
+++ Api.Catalog.Api/Contexts/TenantContext.cs
@@ -1,29 +1,27 @@
 ﻿using Api.Catalog.Api.Constants;
+using Api.Catalog.Api.Models;
 using Api.Catalog.Application.Contracts.Contexts;
 
 namespace Api.Catalog.Api.Contexts;
 
-public sealed class TenantContext(IHttpContextAccessor accessor) : ITenantContext
+public sealed class TenantContext : ITenantContext
 {
-    private readonly bool _isPlatformContext = accessor.HttpContext!.Items[ConstantValues.TenantContextItemKey]?.ToString() == ConstantValues.PlatformContextIdentifier;
-
-    public bool IsPlatformContext => _isPlatformContext;
-    public bool HasHttpContext => accessor.HttpContext is not null;
-    public Guid TenantId
+    private readonly TenantRequestContext _context;
+    public TenantContext(IHttpContextAccessor accessor)
     {
-        get
+        if(
+            accessor.HttpContext?.Items.TryGetValue(ConstantValues.TenantContextItemKey, out var value) == true &&
+            value is TenantRequestContext context
+        )
         {
-            if (!HasHttpContext || IsPlatformContext)
-                return Guid.Empty;
-
-            var _tenantItem = accessor.HttpContext!.Items[ConstantValues.TenantContextItemKey];
-            if (_tenantItem is Guid tenantId)
-                return tenantId;
-
-            if (_tenantItem is not null && Guid.TryParse(_tenantItem.ToString(), out var parsedTenantId))
-                return parsedTenantId;
-
-            throw new InvalidOperationException("Tenant ID não foi resolvido para a requisição.");
+            _context = context;
+        }
+        else
+        {
+            _context = TenantRequestContext.Empty;
         }
     }
+    public bool IsPlatformContext => _context.IsPlatformContext;
+    public bool HasContext => _context.TenantId is not null || _context.IsPlatformContext;
+    public Guid TenantId => _context.TenantId ?? throw new InvalidOperationException("Tenant ID não foi resolvido para a requisição ou acessado em um contexto inválido.");
 }
