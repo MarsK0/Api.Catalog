03c6d048e90efa642696bd897b06762636364249
Marco <marcosilhaag31@gmail.com>
2026-07-10 16:18:12 -0300

    Revert ":recycle: refatoraçao contexto de tenant #2"
    This reverts commit 3ce4ed43fb2cd672abe3e1d399fd1f226def62b7.

--- Api.Catalog.Api/Models/TenantRequestContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Api.Catalog.Api.Models;
-
-public record TenantRequestContext(Guid? TenantId, bool IsPlatformContext)
-{
-    public static readonly TenantRequestContext Empty = new(null, false);
-};
