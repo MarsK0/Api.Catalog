ebd83217e8ce97ec508504f83c9a6eeee67e3a73
Marco <marcosilhaag31@gmail.com>
2026-07-10 16:18:43 -0300

    :building_construction: Abstrai serviço de cache. Criação de interface ICacheService

---
+++ Api.Catalog.Infrastructure/Persistence/Cache/AppCacheService.cs
@@ -0,0 +1,15 @@
+﻿using Api.Catalog.Infrastructure.Contracts;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Api.Catalog.Infrastructure.Persistence.Cache;
+
+internal sealed class AppCacheService : ICacheService
+{
+    private readonly IMemoryCache _cache;
+    public AppCacheService(
+        IMemoryCache cache
+    )
+    {
+        _cache = cache;
+    }
+}
