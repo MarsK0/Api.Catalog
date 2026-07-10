44f0500e7fa827acc7432341be0985e8ec449776
Marco <marcosilhaag31@gmail.com>
2026-07-10 16:18:11 -0300

    :recycle: refatoraçao contexto de tenant #2

--- Api.Catalog.Api/Constants/ConstantValues.cs
+++ Api.Catalog.Api/Constants/ConstantValues.cs
@@ -3,7 +3,7 @@
 public static class ConstantValues
 {
     public const string TenantHeaderName = "Catalog-Tenant-Context";
-    public const string TenantContextItemKey = "tenant_id";
+    public const string TenantContextItemKey = "Tenant-Context";
     public const string PlatformContextIdentifier = "PLATFORM_CONTEXT_ID";
     public const string PlatformSlug = "ctlg-platform";
 }
