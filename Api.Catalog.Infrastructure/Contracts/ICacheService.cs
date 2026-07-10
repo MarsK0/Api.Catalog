49816dca9dbe2d2dc23c1df81bc2258c49cd11a2
Marco <marcosilhaag31@gmail.com>
2026-07-10 16:18:44 -0300

    :bricks: Implementação serviço de cache Microsoft.Extensions.Caching.Hybrid. Remove interface de métodos set e get.

--- Api.Catalog.Infrastructure/Contracts/ICacheService.cs
+++ Api.Catalog.Infrastructure/Contracts/ICacheService.cs
@@ -7,7 +7,5 @@ public record CacheOptions(
 public interface ICacheService
 {
     Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, CacheOptions? options = null);
-    Task<T?> GetAsync<T>(string key);
-    Task SetAsync<T>(string key, T value, CacheOptions? options = null);
     Task RemoveAsync(string key);
 }
--- Api.Catalog.Infrastructure/Persistence/Cache/AppCacheService.cs
+++ Api.Catalog.Infrastructure/Persistence/Cache/AppCacheService.cs
@@ -1,15 +1,47 @@
 ﻿using Api.Catalog.Infrastructure.Contracts;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Api.Catalog.Infrastructure.Persistence.Cache;
 
 internal sealed class AppCacheService : ICacheService
 {
-    private readonly IMemoryCache _cache;
+    private readonly TimeProvider _timeProvider;
+    private readonly HybridCache _cache;
     public AppCacheService(
-        IMemoryCache cache
+        TimeProvider timeProvider,
+        HybridCache cache
     )
     {
+        _timeProvider = timeProvider;
         _cache = cache;
+    }
+    public async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, CacheOptions? options = null)
+    {
+        return await _cache.GetOrCreateAsync(
+            key,
+            _ => new ValueTask<T>(factory()),
+            MapOptions(options) 
+        );
+    }
+
+    public async Task RemoveAsync(string key) => await _cache.RemoveAsync(key);
+
+    private HybridCacheEntryOptions MapOptions(CacheOptions? options)
+    {
+        if(
+            options is null || 
+            (
+                options.AbsoluteExpiration is null && 
+                options.RelativeExpiration is null
+            )
+        )
+            return new HybridCacheEntryOptions();
+
+        return new HybridCacheEntryOptions
+        {
+            Expiration = options.AbsoluteExpiration.HasValue
+                ? options.AbsoluteExpiration.Value - _timeProvider.GetUtcNow()
+                : options.RelativeExpiration
+        };
     }
 }
