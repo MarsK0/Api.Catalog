25a014593f0e38d1d992ef1170edf8ff772a675d
Marco <marcosilhaag31@gmail.com>
2026-07-21 12:55:45 -0300

    :poop: Implementação person context

---
+++ Api.Catalog.Api/Contexts/HttpPersonContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Catalog.Application.Contracts.Contexts;
+
+namespace Api.Catalog.Api.Contexts;
+
+public class HttpPersonContext : IPersonContext
+{
+    private readonly Guid? _personId;
+    public HttpPersonContext(IHttpContextAccessor accessor)
+    {
+        var id = accessor.HttpContext?.User?.FindFirst("sub")?.Value;
+        _personId = Guid.TryParse(id, out var userId) ? userId : null; 
+    }
+    public Guid? PersonId => _personId;
+}
