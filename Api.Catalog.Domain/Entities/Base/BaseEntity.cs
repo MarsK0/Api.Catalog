6358b156c202100a7e6c9b70c0cd0d8549027e54
Marco <marcosilhaag31@gmail.com>
2026-07-23 01:38:32 -0300

    :boom: Remove responsabilidade de trackings da entidade base do domínio

--- Api.Catalog.Domain/Entities/Base/BaseEntity.cs
+++ Api.Catalog.Domain/Entities/Base/BaseEntity.cs
@@ -1,14 +1,6 @@
-﻿using Api.Catalog.Domain.Enums;
-
-namespace Api.Catalog.Domain.Entities;
+﻿namespace Api.Catalog.Domain.Entities;
 
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
-    public EStatus Status { get; protected set; } = EStatus.Enabled;
-    public DateTimeOffset CreatedAt { get; protected set; }
-    public DateTimeOffset? UpdatedAt { get; protected set; }
-
-    protected void Enable() => Status = EStatus.Enabled;
-    protected void Disable() => Status = EStatus.Disabled;
 }
