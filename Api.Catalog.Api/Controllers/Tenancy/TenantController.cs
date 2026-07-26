eeeab8e738b60aa1715477f685cf7f9bb2a919c4
Marco <marcosilhaag31@gmail.com>
2026-07-26 18:54:05 -0300

    :bricks: Endpoint de validação existência slug

--- Api.Catalog.Api/Controllers/Tenancy/TenantController.cs
+++ Api.Catalog.Api/Controllers/Tenancy/TenantController.cs
@@ -1,8 +1,10 @@
 ﻿using Api.Catalog.Api.Authorization;
 using Api.Catalog.Api.Constants;
+using Api.Catalog.Application.Contracts.Contexts;
 using Api.Catalog.Application.Models;
 using Api.Catalog.Domain;
 using Mediator;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Catalog.Api.Controllers.Tenancy;
@@ -11,6 +13,15 @@ namespace Api.Catalog.Api.Controllers.Tenancy;
 [Route("api/tenancy")]
 public class TenantController(IMediator mediator) : CatalogBaseController
 {
+    [HttpGet("slugexists")]
+    [AllowAnonymous]
+    public IActionResult SlugExists(ITenantContext tenantContext, CancellationToken ct)
+    {
+        if (tenantContext.TenantId is not null || tenantContext.IsPlatformContext)
+            return Ok();
+        else
+            return HandleFailure(AppFailure.EntityNotFound("Empresa não encontrada para o Slug informado"));
+    }
     [HttpGet("tenant/{id:guid}")]
     [RequirePermission(Permissions.PlatformPermissions.Tenants.Read)]
     public async Task<IActionResult> GetById(Guid id, CancellationToken ct)
@@ -21,6 +32,7 @@ public class TenantController(IMediator mediator) : CatalogBaseController
                 HandleFailure
             );
     }
+
     [HttpPost("tenant")]
     [RequirePermission(Permissions.PlatformPermissions.Tenants.Create)]
     public async Task<IActionResult> Create([FromBody] CreateTenantCommand command, CancellationToken ct)
